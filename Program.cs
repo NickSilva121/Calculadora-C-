5e1d111455b07ff48791f5589436ae104530ea17
NickSilva121 <pereira.nicolas1210@gmail.com>
2021-07-30 05:51:31 -0300

    primeito programa c sharp

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero1, numero2;
+            String operador;
+
+            Console.Write("Calculadora de Console C#\n");
+
+            Console.Write("Insira um número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a operação: ");
+            operador = Console.ReadLine();
+
+            Console.Write("Insira o segundo valor: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (operador)
+            {
+                case "+":
+                    Console.WriteLine($"Total: {numero1} + {numero2} = " + (numero1 + numero2));
+                    break;
+
+                case "-":
+                    Console.WriteLine($"Total: {numero1} - {numero2} = " + (numero1 - numero2));
+                    break;
+                case "*":
+                    Console.WriteLine($"Total: {numero1} * {numero2} = " + (numero1 * numero2));
+                    break;
+                case "/":
+                    Console.WriteLine($"Total: {numero1} / {numero2} = " + (numero1 / numero2));
+                    break;
+                default:
+                    Console.WriteLine("Valor inválido, tente novamente...");
+                break;
+            }
+
+            Console.WriteLine("Aperte ENTER para fechar");
+            Console.ReadKey();
+        }
+    }
+}
